dc07f2d695e158abd06a60d2168cb30ef2d089ff
Adrian333Dev <adrian333dev@gmail.com>
2023-07-23 03:25:49 +0400

    save changes

--- base/views.py
+++ base/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Room
 # Create your views here.
+
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn Python'},
+#     {'id': 2, 'name': 'Designing a Django App'},
+#     {'id': 3, 'name': 'Django Channels'},
+# ]
 
 
 def home(req):
-    return HttpResponse("Hello World")
+    rooms = Room.objects.all()
+    return render(req, 'base/home.html', {'rooms': rooms})
 
 
-def room(req):
-    return HttpResponse("Hello Room")
+def room(req, pk):
+    room = Room.objects.get(id=pk)
+    return render(req, 'base/room.html', {'room': room})
